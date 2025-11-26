5fb771bb86b36a567a560bdea2d7ea46615b5d79
ivanjtorresr <itorresr@ull.edu.es>
2025-11-26 22:52:47 +0000

    Added gitignore and modified tests

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -3,11 +3,12 @@ from calc import sum_two
 
 class TestCalc(unittest.TestCase):
     def test_sum(self):
-        self.assertEqual(sum_two(2,3), 5.0)
-        self.assertAlmostEqual(sum_two("1.5","2.5"),4.0)
+        self.assertEqual(sum_two(2, 3), 5.0)
+        self.assertAlmostEqual(sum_two("1.5", "2.5"), 4.0)
+
     def test_error(self):
         with self.assertRaises(ValueError):
-            sum_two("a",3)
+            sum_two("a", 3)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     unittest.main()
